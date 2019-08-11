80108bfc3732c1a31c6bcfd8108d8bdf943ebff0
heqi8826 <heqi8826@gmail.com>
2019-08-11 18:18:01 +0800

    Update linear_regression.py

--- 深度学习与TensorFlow入门实战-源码和PPT/lesson04-回归问题实战/linear_regression.py
+++ 深度学习与TensorFlow入门实战-源码和PPT/lesson04-回归问题实战/linear_regression.py
@@ -8,7 +8,7 @@ def compute_error_for_line_given_points(b, w, points):
     totalError = 0
     for i in range(0, len(points)):
         x = points[i, 0]
-        y = points[i, 1]
+        y = points[0, i]
         # computer mean-squared-error
         totalError += (y - (w * x + b)) ** 2
     # average loss for each point
